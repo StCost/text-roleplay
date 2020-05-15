99c62935fb952a7ea8e68c7b3bc8e7366612eac1
Kostia Nazvanov <nazvanov.kst@gmail.com>
2020-05-15 10:36:41 +0300

    Add build

---
+++ docs/precache-manifest.ddcc9cefd5134503a8f7f691d088f862.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f2351cc1f40ac8a4abd7bde03ddde0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e392e0be1f68de2bcfa",
+    "url": "/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "ee21fd1fa5031815d4e1",
+    "url": "/static/css/main.9c826ed6.chunk.css"
+  },
+  {
+    "revision": "3e392e0be1f68de2bcfa",
+    "url": "/static/js/2.e88edff8.chunk.js"
+  },
+  {
+    "revision": "996e97a9c59b2ad74d9e36e982ab30fe",
+    "url": "/static/js/2.e88edff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee21fd1fa5031815d4e1",
+    "url": "/static/js/main.d2b1662d.chunk.js"
+  },
+  {
+    "revision": "7c1beb613db0f3d7192f",
+    "url": "/static/js/runtime-main.489cb710.js"
+  }
+]);
