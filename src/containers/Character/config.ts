e8d72c1c2bee61b6169f5f983827a58b603e10aa
St.Cost <nazvanov.kst@gmail.com>
2020-06-13 17:37:00 +0300

    Change Skill Points formula

--- src/containers/Character/config.ts
+++ src/containers/Character/config.ts
@@ -391,7 +391,7 @@ export const subStats: IField[] = [
     label: 'ОН',
     full: 'Очки Навыков',
     field: 'skillPoints',
-    getBase: (s, stats) => Math.floor((stats.level) * (10 + (s.in / 2)) - stats.spentSkillPoints),
+    getBase: (s, stats) => Math.floor((stats.level - 1) * (10 + (s.in / 2)) - stats.spentSkillPoints),
   },
 ];
 
