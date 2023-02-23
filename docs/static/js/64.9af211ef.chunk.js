5b1fa5e86e4aa6ae0d62a07edf8673d17b40bd50
KostyaNazvanov <k.nazvanov@theevenstar.net>
2023-02-24 00:37:46 +0200

    Update build

---
+++ docs/static/js/64.9af211ef.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktext_roleplay=self.webpackChunktext_roleplay||[]).push([[64],{5321:function(e,t,l){l.r(t),t.default={}}}]);
+//# sourceMappingURL=64.9af211ef.chunk.js.map
